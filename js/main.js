86d4342601dfb3ae1056b5ceba127aea52218059
Dylan Cole <54950847+Dylan1217@users.noreply.github.com>
2024-11-12 10:26:49 -0600

    added header page and html files for buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // JS scripts placed here
+
+// Select all links within the navigation
+const navLinks = document.querySelectorAll('.nav-links a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link click behavior if needed
+
+    // Remove the active class from all links
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    // Add the active class to the clicked link
+    this.classList.add('active');
+  });
+});
