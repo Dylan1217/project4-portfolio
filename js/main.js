da750e4268dbc0183f591c808c623d47950370d5
Dylan Cole <54950847+Dylan1217@users.noreply.github.com>
2024-11-12 10:59:27 -0600

    added page navigation

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const navLinks = document.querySelectorAll('.nav-links a');
 
 navLinks.forEach(link => {
   link.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default link click behavior if needed
+    event.preventDefault(); // Prevent default link behavior if needed
 
     // Remove the active class from all links
     navLinks.forEach(link => link.classList.remove('active'));
