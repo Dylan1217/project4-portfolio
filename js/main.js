67b0a60d0c2833b43543dfe5353fbc1ac97e2fe9
Dylan Cole <54950847+Dylan1217@users.noreply.github.com>
2024-11-26 10:27:45 -0600

    added carousel into projects page

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@
 // Select all links within the navigation
 const navLinks = document.querySelectorAll('.nav-links a');
 
-navLinks.forEach(link => {
-  link.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default link behavior if needed
+// navLinks.forEach(link => {
+//   link.addEventListener('click', function(event) {
+//     event.preventDefault(); // Prevent default link behavior if needed
 
-    // Remove the active class from all links
-    navLinks.forEach(link => link.classList.remove('active'));
+//     // Remove the active class from all links
+//     navLinks.forEach(link => link.classList.remove('active'));
 
-    // Add the active class to the clicked link
-    this.classList.add('active');
-  });
-});
+//     // Add the active class to the clicked link
+//     this.classList.add('active');
+//   });
+// });
 
 
   $(document).ready(function () {
