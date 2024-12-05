680b6b5ade2b257220add0273af0235d851e32f0
Dylan Cole <dylan1217@mindspring.com>
2024-12-05 01:46:08 -0600

    updated the first project

--- js/main.js
+++ js/main.js
@@ -16,13 +16,24 @@ const navLinks = document.querySelectorAll('.nav-links a');
 // });
 
 
-  $(document).ready(function () {
-    $('.pic-carousel').slick({
-        slidesToShow: 1,         // Number of slides visible at once
-        slidesToScroll: 1,       // Number of slides to scroll at a time
-        autoplay: false,          // Enable auto-sliding
-        autoplaySpeed: 2000,     // Time between slides
-        dots: true,              // Show navigation dots
-        centerMode: true         // Center the active slide
-    });
+$(document).ready(function () {
+  $('.pic-carousel').slick({
+    slidesToShow: 1,         // Number of slides visible at once
+    slidesToScroll: 1,       // Number of slides to scroll at a time
+    autoplay: false,          // Enable auto-sliding
+    autoplaySpeed: 2000,     // Time between slides
+    dots: true,              // Show navigation dots
+    centerMode: true         // Center the active slide
+  });
+});
+
+$(document).ready(function () {
+  $('.p1-pic-carousel').slick({
+    slidesToShow: 1,         // Number of slides visible at once
+    slidesToScroll: 1,       // Number of slides to scroll at a time
+    autoplay: false,          // Enable auto-sliding
+    autoplaySpeed: 2000,     // Time between slides
+    dots: true,              // Show navigation dots
+    centerMode: true         // Center the active slide
+  });
 });
